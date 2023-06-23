0d2f0ac9acd3bc5cec3bc0cc8b4782d7b5e14892
Simon <1simonsteven@gmail.com>
2023-06-23 19:40:19 +0200

    Users procedures

---
+++ src/server/api/routers/users.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const userRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.user.findMany();
+  }),
+  addUser: publicProcedure
+    .input(z.object({ firstName: z.string(), lastName: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const user = await ctx.prisma.user.findFirst({
+        where: {
+          firstName: input.firstName,
+          lastName: input.lastName
+        }
+      })
+      if (user) {
+        return
+      }
+      return await ctx.prisma.user.create({
+        data: {
+          firstName: input.firstName,
+          lastName: input.lastName,
+          totalGames: 0,
+          rating: 0
+        }
+      })
+    }),
+  deleteUser: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.user.delete({
+        where: {
+          id: input.id
+        }
+      })
+    })
+});
