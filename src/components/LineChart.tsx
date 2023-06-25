09fd33e93ca25608d31824e8ae864a928be14e5b
Simon <1simonsteven@gmail.com>
2023-06-25 17:09:54 +0200

    Created line chart component

---
+++ src/components/LineChart.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+type ChartPropTypes = {
+  options: {
+    responsive: boolean;
+    maintainAspectRatio: boolean;
+    plugins: {
+      legend: {
+        position: "top";
+      };
+      title: {
+        display: boolean;
+        text: string;
+      };
+    };
+  },
+  data: {
+    labels: string[];
+    datasets: {
+      label: string;
+      data: number[];
+      borderColor: string | undefined;
+      backgroundColor: string | undefined;
+    }[];
+  }
+}
+
+export default function Chart({ options, data }: ChartPropTypes) {
+  return <Line options={options} data={data} />;
+}
