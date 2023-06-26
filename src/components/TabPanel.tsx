0b24f2d548f30afb85d7e830eb36c49a907ab4b5
Simon <1simonsteven@gmail.com>
2023-06-26 20:53:50 +0200

    Create tab component

---
+++ src/components/TabPanel.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+type TabProps = {
+  tabs: {
+    title: string,
+    content: React.ReactElement
+  }[]
+}
+
+export default function BasicTabs({ tabs }: TabProps) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange}>
+          {tabs.map((tab, index) => {
+            return <Tab label={tab.title} {...a11yProps(index)} />
+          })}
+        </Tabs>
+      </Box>
+      {tabs.map((tab, index) => {
+        return <TabPanel value={value} index={index}>
+          {tab.content}
+        </TabPanel>
+      })}
+    </Box>
+  );
+}
