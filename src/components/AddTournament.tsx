52ffb739c19eff6842307d911a382ac77380e421
Simon <1simonsteven@gmail.com>
2023-06-29 07:14:09 +0200

    Add tournaments now has more options

--- src/components/AddTournament.tsx
+++ src/components/AddTournament.tsx
@@ -2,18 +2,27 @@ import { RouterOutputs } from "~/server/api/trpc"
 import TransferList from "./TransferList"
 import { useEffect, useState } from "react"
 import { api } from "~/utils/api"
-import { Button, Stack, TextField, Typography } from "@mui/material"
+import { Box, Button, Checkbox, FormControlLabel, InputLabel, MenuItem, Select, Stack, TextField, Typography } from "@mui/material"
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import React from "react"
+import dayjs, { Dayjs } from 'dayjs'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from "@mui/x-date-pickers"
 
 type UserType = RouterOutputs['user']['getAll']
 type AddTournamentProps = {
   handleSubmit: () => void
   handleCancel: () => void
 }
+
 export default function AddTournament({ handleSubmit, handleCancel }: AddTournamentProps) {
   const { data: users } = api.user.getAll.useQuery()
   const [left, setLeft] = useState<UserType>([])
   const [right, setRight] = useState<UserType>([])
   const [tournamentName, setTournamentName] = useState('')
+  const [startDate, setStartDate] = React.useState<Dayjs | null>(dayjs());
+  const [roundInterval, setRoundInterval] = useState<string>('week')
+  const [emailReminders, setEmailReminders] = useState(false)
   const utils = api.useContext()
   const { mutate: createTournament } = api.tournament.createTournament.useMutation({
     async onSuccess() {
@@ -28,11 +37,37 @@ export default function AddTournament({ handleSubmit, handleCancel }: AddTournam
     setLeft(users ?? [])
   }, [users])
 
+
   return <Stack spacing={2}>
-    <TextField
-      label='Tournament name'
-      value={tournamentName}
-      onChange={e => setTournamentName(e.target.value)}
+    <Box>
+      <TextField
+        label='Tournament name'
+        value={tournamentName}
+        onChange={e => setTournamentName(e.target.value)}
+      />
+    </Box>
+    <Box>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker label="Tournament start date" value={startDate} onChange={newVal => setStartDate(newVal)} />
+      </LocalizationProvider>
+    </Box>
+    <Box>
+      <InputLabel sx={{ marginBottom: '5px' }}>Round Frequency</InputLabel>
+      <Select
+        label='Round Frequency'
+        value={roundInterval}
+        onChange={e => setRoundInterval(e.target.value)}
+      >
+        <MenuItem value='day'>Daily</MenuItem>
+        <MenuItem value='week'>Weekly</MenuItem>
+      </Select>
+    </Box>
+    <FormControlLabel
+      control={<Checkbox
+        value={emailReminders}
+        onChange={e => setEmailReminders(e.target.checked)}
+      />}
+      label="Email reminders"
     />
     <Stack spacing={1}>
       <Typography variant="h5">
@@ -51,8 +86,13 @@ export default function AddTournament({ handleSubmit, handleCancel }: AddTournam
       <Button
         variant="outlined"
         onClick={() => {
-          const playerIds = right.map(user => user.id)
-          createTournament({ name: tournamentName, playerIds })
+          createTournament({
+            name: tournamentName,
+            playerIds: right.map(user => user.id),
+            emailReminders,
+            startDate: startDate?.toDate() ?? new Date(),
+            roundInterval
+          })
           handleSubmit()
         }}
       >
