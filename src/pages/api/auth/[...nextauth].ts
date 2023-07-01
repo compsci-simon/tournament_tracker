2650ba2dab40d5929f35b0ea1c198bbd06f364c5
Simon <1simonsteven@gmail.com>
2023-07-01 19:18:25 +0200

    Modified nextauth to actually work

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,16 @@
+import { TRPCError } from "@trpc/server";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from 'next-auth/providers/credentials'
+import { api } from "~/utils/api";
+import { prisma } from "~/server/db";
+import z from 'zod'
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1).max(12)
+})
+
+type ILogin = z.infer<typeof loginSchema>
 
 export default NextAuth({
   session: {
@@ -9,19 +20,26 @@ export default NextAuth({
     CredentialsProvider({
       name: 'Credentials',
       credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'example@test.com' },
+        email: { Email: 'Email', type: 'text', placeholder: 'example@test.com' },
         password: { label: 'Password', type: 'password' }
       },
       async authorize(credentials, req) {
-        if (credentials?.username == 'simon') {
-          return {
-            id: '1',
-            name: 'simon',
-            email: 'simon.steven@hexagon.com',
-            image: ''
+        // const creds = await loginSchema.parseAsync(credentials)
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials?.email ?? '',
+            password: credentials?.password ?? ''
           }
+        })
+        if (!user) {
+          return null
         }
-        return null
+        return {
+          id: user.id,
+          name: user.nickName ?? `${user.firstName} ${user.lastName}`,
+          email: user.email,
+          image: user.avatar
+        }
       },
     })
   ],
