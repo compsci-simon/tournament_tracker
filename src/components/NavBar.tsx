d071c0fa686d1ba91069093740002e74780f3801
Simon <1simonsteven@gmail.com>
2023-07-01 19:19:17 +0200

    Added personal avatar to navba as well as a menu from avater

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,24 +10,36 @@ import Container from '@mui/material/Container';
 import MenuItem from '@mui/material/MenuItem';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import Link from 'next/link';
-import { Button, Stack } from '@mui/material';
+import { Avatar, Button, Stack, Tooltip } from '@mui/material';
 import { useRouter } from 'next/router';
 import ThemeSwitch from './ThemeSwitch';
 import { ThemeContext } from '~/pages/_app';
+import { useSession } from 'next-auth/react';
 
 const pages = ['Tournaments', 'Players', 'Quick-Game'];
+const settings = ['Profile', 'Logout'];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
   const router = useRouter()
   const { dark, setDark } = React.useContext(ThemeContext)
+  const { data: session } = useSession()
+  console.log('session', session)
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
   };
 
   return (
@@ -115,6 +127,38 @@ function ResponsiveAppBar() {
           </Box>
 
           <ThemeSwitch defaultChecked value={dark} onChange={() => setDark(!dark)} />
+
+          <Box sx={{ flexGrow: 0, ml: '15px' }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src={session?.user?.image ?? ''} />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Link href={`/${setting.toLowerCase()}`}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
 
         </Toolbar>
       </Container>
