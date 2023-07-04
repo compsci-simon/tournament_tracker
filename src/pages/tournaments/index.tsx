b8b6ed7b7e0dc4fde39165a650201977b8fff205
Simon <1simonsteven@gmail.com>
2023-07-04 21:59:22 +0200

    Updated tournaments page to show upcoming tournaments

--- src/pages/tournaments/index.tsx
+++ src/pages/tournaments/index.tsx
@@ -18,33 +18,50 @@ const options = {
   },
 }
 
-export default function Page() {
+export default function TournamentsPage() {
   const { data: tournaments } = api.tournament.tournamentsStats.useQuery()
   const router = useRouter()
+  const currentDate = new Date()
 
   return <Box padding={4}>
     {tournaments?.map(tournament => {
+      const started = tournament.startDate <= currentDate
       return <Paper key={tournament.id}>
         <Box padding={2}>
           <Stack spacing={1}>
-            <Typography variant='h6'>
+            <Typography variant='h3'>
               {tournament.name}
             </Typography>
-            <Box>
-              <Button
-                variant='outlined'
-                onClick={() => { void router.push(`/tournaments/${tournament.id}`).then((r) => { r }).catch(e => console.log(e)) }}
-              >
-                View
-              </Button>
-            </Box>
-            <Box height={300}>
-              {
-                tournament.chartData ?
-                  <LineChart options={options} data={tournament.chartData} />
-                  : null
-              }
-            </Box>
+            {
+              !started ?
+                <Typography variant='h5'>
+                  Starting: {tournament.startDate.toDateString()}
+                </Typography>
+                : null
+            }
+            {
+              started ?
+                <>
+                  <Box height={300}>
+                    {
+                      tournament.chartData ?
+                        <LineChart options={options} data={tournament.chartData} />
+                        : null
+                    }
+                  </Box>
+                  <Box>
+                    <Button
+                      variant='outlined'
+                      onClick={() => { void router.push(`/tournaments/${tournament.id}`).then((r) => { r }).catch(e => console.log(e)) }}
+                    >
+                      View
+                    </Button>
+                  </Box>
+                </>
+                : <Box>
+                  <Button variant='outlined'>Join</Button>
+                </Box>
+            }
           </Stack>
         </Box>
       </Paper>
@@ -52,8 +69,10 @@ export default function Page() {
   </Box>
 }
 
-Page.getLayout = function getLayout(page: React.ReactElement) {
+TournamentsPage.getLayout = function getLayout(page: React.ReactElement) {
   return <Layout>
     {page}
   </Layout>
 }
+
+TournamentsPage.auth = true
