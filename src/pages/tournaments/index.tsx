a1b6dfb8bb4f11211e0513ac33be974fcd624f23
Simon <1simonsteven@gmail.com>
2023-07-05 21:02:14 +0200

    Added buttons to join and leave a tournament

--- src/pages/tournaments/index.tsx
+++ src/pages/tournaments/index.tsx
@@ -3,6 +3,8 @@ import Layout from '../../components/Layout'
 import LineChart from '../../components/LineChart'
 import { api } from '~/utils/api'
 import { useRouter } from 'next/router'
+import { enqueueSnackbar } from 'notistack'
+import { useSession } from 'next-auth/react'
 
 const options = {
   responsive: true,
@@ -19,13 +21,69 @@ const options = {
 }
 
 export default function TournamentsPage() {
+  const utils = api.useContext()
   const { data: tournaments } = api.tournament.tournamentsStats.useQuery()
+  const { mutate: joinTournamentMutation } = api.tournament.joinTournament.useMutation({
+    onSuccess() {
+      utils.tournament.tournamentsStats.invalidate()
+      enqueueSnackbar('Joined tournament', { variant: 'success' })
+    },
+    onError() {
+      enqueueSnackbar('Failed to join tournament', { variant: 'error' })
+    }
+  })
+  const { mutate: leaveTournamentMutation } = api.tournament.leaveTournament.useMutation({
+    onSuccess() {
+      utils.tournament.tournamentsStats.invalidate()
+      enqueueSnackbar('Left tournament', { variant: 'success' })
+    },
+    onError() {
+      enqueueSnackbar('Failed to leave tournament', { variant: 'error' })
+    }
+  })
+  const { data: session } = useSession()
   const router = useRouter()
   const currentDate = new Date()
 
   return <Box padding={4}>
     {tournaments?.map(tournament => {
       const started = tournament.startDate <= currentDate
+      const joinTournament = () => {
+        joinTournamentMutation({ tournamentId: tournament.id })
+      }
+      const leaveTournament = () => {
+        leaveTournamentMutation({ tournamentId: tournament.id })
+      }
+
+      const ButtonToShow = () => {
+        const signedUpToTournament = tournament.players.map(p => p.email).includes(session?.user.email ?? '')
+        if (!signedUpToTournament) {
+          return <>
+            <Typography fontSize={12}>You are <b>not</b> set to take part in this tournament!</Typography>
+            <Box>
+              <Button
+                variant='outlined'
+                onClick={joinTournament}
+              >
+                Join
+              </Button>
+            </Box>
+          </>
+        }
+        return <>
+          <Typography fontSize={12}>You are set to take part in this tournament!</Typography>
+          <Box>
+            <Button
+              variant='outlined'
+              onClick={leaveTournament}
+              color='error'
+            >
+              Leave tournament
+            </Button>
+          </Box>
+        </>
+      }
+
       return <Paper key={tournament.id}>
         <Box padding={2}>
           <Stack spacing={1}>
@@ -58,9 +116,7 @@ export default function TournamentsPage() {
                     </Button>
                   </Box>
                 </>
-                : <Box>
-                  <Button variant='outlined'>Join</Button>
-                </Box>
+                : ButtonToShow()
             }
           </Stack>
         </Box>
