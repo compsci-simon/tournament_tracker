4285dd27f58d0622e1406a30b927b617a7fbcc2a
Simon <1simonsteven@gmail.com>
2023-07-21 07:07:31 +0200

    Added trailing newline to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import NavBar from './NavBar'
-import { useSession } from 'next-auth/react'
 
 export default function PageLayout({
   children
@@ -12,7 +11,7 @@ export default function PageLayout({
       <Head>
         <title>League</title>
         <meta name="description" content="Track your office tournament" />
-        <link rel="icon" href="/logo.svg" />
+        <link rel="icon" href='/logo.svg' />
       </Head>
       <main>
         <NavBar />
