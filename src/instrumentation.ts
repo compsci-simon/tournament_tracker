5bc4d156f884549a8a29fe42c900c42a68dbad5e
Simon <1simonsteven@gmail.com>
2023-07-22 18:48:55 +0200

    Games can be either round-robbin or multi-stage. Startup handles creation of both

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,5 @@
 import { prisma } from "./server/db"
-import { roundRobinScheduleGames } from "./utils/tournament"
+import { roundRobinScheduleGames, scheduleMultiStageGames } from "./utils/tournament"
 
 const startTournament = async (tournamentId: string) => {
   const tournament = await prisma.tournament.findFirst({
@@ -10,27 +10,61 @@ const startTournament = async (tournamentId: string) => {
       players: true
     }
   })
-  const { schedule, numRounds } = roundRobinScheduleGames(tournament.players.map(p => p.id))
-  return await prisma.tournament.update({
-    where: {
-      id: tournamentId
-    },
-    data: {
-      games: {
-        create: schedule.map(s => {
-          return ({
-            players: {
-              connect: s.player2 ? [({ id: s.player1 }), ({ id: s.player2 })] : [({ id: s.player1 })]
-            },
-            player1Id: s.player1,
-            player2Id: s.player2 ?? '',
-            round: s.round
-          })
-        })
+  if (tournament.type == 'round-robbin') {
+    const { schedule, numRounds } = roundRobinScheduleGames(tournament.players.map(p => p.id))
+
+    return await prisma.tournament.update({
+      where: {
+        id: tournamentId
       },
-      numRounds
-    }
-  })
+      data: {
+        games: {
+          create: schedule.map(s => {
+            return ({
+              players: {
+                connect: s.player2 ? [({ id: s.player1 }), ({ id: s.player2 })] : [({ id: s.player1 })]
+              },
+              player1Id: s.player1,
+              player2Id: s.player2 ?? '',
+              round: s.round
+            })
+          })
+        },
+        numRounds
+      }
+    })
+  } else if (tournament.type == 'multi-stage') {
+    const { gameSchedule, numRounds } = scheduleMultiStageGames(tournament.players.map(p => p.id))
+
+    return await prisma.tournament.update({
+      where: {
+        id: tournamentId,
+      },
+      data: {
+        games: {
+          create: gameSchedule.map(game => {
+            return ({
+              players: {
+                connect: game.type == 'group' ?
+                  (
+                    game.player2 ? [({ id: game.player1 }), ({ id: game.player2 })] : [({ id: game.player1 })]
+                  )
+                  : []
+              },
+              player1Id: game.player1 ?? '',
+              player2Id: game.player2 ?? '',
+              round: game.round,
+              type: game.type,
+              group: game.group
+            })
+          })
+        },
+        numRounds
+      }
+    })
+  } else {
+    throw new Error(`Unknown tournament type: ${tournament.type}`);
+  }
 }
 
 const startTournaments = async () => {
