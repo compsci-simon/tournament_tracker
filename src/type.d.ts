b6674c35531d9a0f2fb0bea8ced9563abc2916fb
Simon <1simonsteven@gmail.com>
2024-01-14 09:04:32 +0200

    refactored types to have their own file

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,9 +1,13 @@
-type nodeProps = {
+import { Game, User } from "@prisma/client"
+
+export type nodeProps = {
   id: string,
   data: {
-    date: string | null,
-    player1: string | null,
-    player2: string | null,
-    winner: string | null
+    player1: User | null,
+    player2: User | null,
+    game: (Game & { player1: User, player2: User }) | null
   }
 }
+
+export type TournamentType = RouterOutputs['tournament']['getTournament']
+export type GameType = ElementType<TournamentType['games']>
