7aa0a3d8a331fe457ff7a42ab8ffd7792c9c3f0b
Simon <1simonsteven@gmail.com>
2024-04-26 19:28:48 +0200

    Have an actual test that is useful.

--- src/test/tournament.test.ts
+++ src/test/tournament.test.ts
@@ -4,11 +4,17 @@ import { scheduleMultiStageGames } from "../utils/tournament";
 
 
 const players = ['simon', 'ben', 'lienke', 'liv', 'james', 'lisa', 'shailen', 'paul']
+
 describe('test scheduling functions', () => {
 
   test('Test scheduling results in a good split of pools', () => {
     const { gameSchedule, numRounds } = scheduleMultiStageGames(players)
-    expect(true).toBe(true)
+    expect(numRounds).toBe(3)
+    expect(gameSchedule.length).toBe(19)
+    expect(gameSchedule.filter(g => g.type == 'knockout').length).toBe(7)
+    expect(gameSchedule.filter(g => g.type == 'knockout' && g.level == 0).length).toBe(4)
+    expect(gameSchedule.filter(g => g.type == 'knockout' && g.level == 1).length).toBe(2)
+    expect(gameSchedule.filter(g => g.type == 'knockout' && g.level == 2).length).toBe(1)
   })
 
 })
